fc97f044636b644ef223f793171e6013e6931550
Kateryna Masiuk <kmasiuk91@gmail.com>
2024-12-12 12:47:38 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import CampsitesList from './features/campsites/CampsitesList.js';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+    return (
+    <div className='App'>
+      <Header />
+      <CampsitesList />
+      <Footer />
+      </div>
+    );
+}
+
+export default App;
